4fac09bd1ed18652eef4cbfcf4fa24ed5453b8f8
st107748 <masha.abakarova@gmail.com>
2022-09-25 16:04:14 +0300

    1 hm

--- 2022.09.12-Homework-1/Task1/Source.cpp
+++ 2022.09.12-Homework-1/Task1/Source.cpp
@@ -1,15 +1,19 @@
 #include<iostream>
 
-using namespace std;
+
+#include <locale.h>
 
 int main(int argc, char* argv[])
 {
+	
+	setlocale(LC_ALL, "Russian");
+
 	int a = 0;
+	std::cout << "¬ведите целое число, по модулю не превосход€щее 10000" << std::endl;
+	std::cin >> a;
 
-	cin >> a;
-
-	cout << "The next number for the number " << a << " is " << a + 1 << "." << endl;
-	cout << "The previous number for the number " << a << " is " << a - 1 << "." << endl;
+	std::cout << "The next number for the number " << a << " is " << a + 1 << "." << std::endl;
+	std::cout << "The previous number for the number " << a << " is " << a - 1 << "." << std::endl;
 
 	return EXIT_SUCCESS;
 }
