fa46d28c370e2f74b19666d06feaa11c232ebf77
st107748 <masha.abakarova@gmail.com>
2022-10-01 13:02:34 +0300

    123

--- 2022.09.12-Homework-1/Task8/Source.cpp
+++ 2022.09.12-Homework-1/Task8/Source.cpp
@@ -1,12 +1,7 @@
 #include<iostream>
 
-
-#include <locale.h>
-
 int main(int argc, char* argv[])
 {
-	setlocale(LC_ALL, "Russian");
-
 	int a = 0;
 	int aa = 0;
 	int aaa = 0;
@@ -14,10 +9,9 @@ int main(int argc, char* argv[])
 	int bb = 0;
 	int bbb = 0;
 
-	std::cout << "Введите значения двух моментов времени, принадлежащих одним и тем же суткам: часы, минуты и секунды для каждого из моментов времени" << std::endl;
 	std::cin >> a >> aa >> aaa >> b >> bb >> bbb;
 
-	std::cout << "Число секунд между этими моментами времени: " << (b * 3600 + bb * 60 + bbb) - (a * 3600 + aa * 60 + aaa) << std::endl;
+	std::cout << (b * 3600 + bb * 60 + bbb) - (a * 3600 + aa * 60 + aaa) << std::endl;
 
 	return EXIT_SUCCESS;
 }
