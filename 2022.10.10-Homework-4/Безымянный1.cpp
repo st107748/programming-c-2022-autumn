e19853329d90cbcdd7897bbbb9c552540726cbdb
st107748 <masha.abakarova@gmail.com>
2022-10-30 01:03:54 +0300

    267

---
+++ 2022.10.10-Homework-4/Безымянный1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+int main(int argc, char* argv[])
+{
+	int x = 0;
+	int a = 0;
+
+	std::cin >> x;
+
+	for (int i = 1; i <= x; ++i)
+	{
+		if (x % i == 0)
+		{
+			 ++a;
+		}
+	}
+	std::cout << a;
+	system("pause");
+	
+	return EXIT_SUCCESS;
+}
