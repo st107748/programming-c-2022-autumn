94215468c9bc047a694e959daf1bd4e91000f847
st107748 <masha.abakarova@gmail.com>
2023-09-21 00:38:48 +0300

    038

---
+++ 13.09.2023-Homework-2/Task2/Source.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<string>
+#include<fstream>
+#include<sstream>
+
+void function()
+{
+	std::string h;
+	int x;
+	int sum = 0;
+	std::string s;
+	std::ifstream f("ff.txt");
+	std::ofstream g("gg.txt");
+	/* while (getline(f, h) >> x)
+	{
+		sum = sum + x;
+		
+		g << sum << std::endl;
+
+		sum = 0;
+	}
+	*/
+
+	while (getline(f, h))
+	{
+		std::stringstream str(h);
+		while (str >> x)
+		{
+			sum = sum + x;
+		}
+		g << sum << std::endl;
+		sum = 0;
+	}
+}
+int main(int argc, char* argv[])
+{
+	function();
+	return EXIT_SUCCESS;
+}
